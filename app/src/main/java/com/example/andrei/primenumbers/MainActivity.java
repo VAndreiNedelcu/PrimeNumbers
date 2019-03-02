80c9f84fec55e65e5fa615d6e25864accdb74726
Andrei <andrei_nedelcu28@yahoo.com>
2019-03-02 13:02:54 +0200

    initial commit

--- app/src/main/java/com/example/andrei/primenumbers/MainActivity.java
+++ app/src/main/java/com/example/andrei/primenumbers/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.andrei.primenumbers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    Button primeButton = (Button) findViewById(R.id.primeButton);
+        primeButton.setOnClickListener(
+                new Button.OnClickListener(){
+        public void onClick(View v){
+            TextView primeText = (TextView) findViewById(R.id.primeText);
+            int max =  500;
+            for(int i=2; i<=max; i++) {
+                boolean isPrimeNumber = true;
+                for (int j = 2; j <= i; j++) {
+                    if (i % j == 0 ) {
+                        isPrimeNumber = false;
+                        break;
+                    }
+
+                }
+                if (isPrimeNumber){
+                    primeText.setText(Integer.toString(i));
+                }
+            }
+        }
     }
+        );
+}
 }
